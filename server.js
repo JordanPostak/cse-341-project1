19e47593e8260f06a1a877a1e9a65e2e6768f7bd
JordanPostak <jordan_postak@hotmail.com>
2024-01-23 10:26:50 -0700

    fixed typo

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
         'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Tye, Accept, Z-Key'
+        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
     );
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
