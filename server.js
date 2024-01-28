64e101aa4fd88f8548a2016c2577a23b059b7191
JordanPostak <jordan_postak@hotmail.com>
2024-01-27 18:40:40 -0700

    no cors

--- server.js
+++ server.js
@@ -3,30 +3,84 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const passport = require('passport');
+const GitHubStrategy = require('passport-github2').Strategy;
 const mongodb = require('./data/database');
-const app = express();
 
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// CORS headers setup
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
         'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+        'Origin, X-Requested-With, Content-Type, Accept, Z-Key, Authorization'
     );
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader(
+        'Access-Control-Allow-Methods', 
+        'POST, GET, PUT, PATCH, OPTIONS, DELETE'
+    );
     next();
 });
-app.use('/', require('./routes'));
 
-mongodb.initDb((err) => {
-    if(err) {
-        console.log(err);
-    }
-    else {
-        app.listen(port, ()=> {console.log(`Database is listening and node Running on port ${port}`)});
+app.use("/", require("./routes/index.js"));
+
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.CALLBACK_URL
+}, (accessToken, refreshToken, profile, done) => {
+    // User.findOrCreate({ githubId: profile.id }, function (err, user) {
+    return done(null, profile);
+}));
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+
+app.get('/', (req, res) => {
+    if (req.session.user !== undefined) {
+        const displayName = req.session.user.displayName;
+        const profilePicture = req.session.user.photos[0].value;
+        res.send(`
+            <h1>Logged in as ${displayName}</h1>
+            <img src="${profilePicture}" alt="Profile Picture">
+        `);
+    } else {
+        res.send("Logged Out");
     }
 });
 
+app.get('/github/callback', passport.authenticate('github', {
+    failureRedirect: '/api-docs', session: false
+}), (req, res) => {
+    req.session.user = req.user;
+    res.redirect('/');
+});
+
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port, () => {
+            console.log(`Database is listening and node Running on port ${port}`);
+        });
+    }
+});
 
